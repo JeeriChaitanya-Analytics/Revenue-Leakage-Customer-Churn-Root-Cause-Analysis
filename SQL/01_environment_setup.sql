8ab7a1dea1347faed05fb713a85e85c2f13280cc
✅ Jeeri chaitanya <chaituchaitanya347@gmail.com>
2026-02-25 22:54:03 +0530

    Update 01_environment_setup.sql

--- SQL/01_environment_setup.sql
+++ SQL/01_environment_setup.sql
@@ -1,1 +1,19 @@
+=====================================================
+STEP 1: Environment Setup
+Creates warehouse, database, and schema
+=====================================================
 
+CREATE OR REPLACE WAREHOUSE retail_wh
+WITH WAREHOUSE_SIZE = 'XSMALL'
+AUTO_SUSPEND = 60
+AUTO_RESUME = TRUE;
+
+USE WAREHOUSE retail_wh;
+
+CREATE OR REPLACE DATABASE retail_analytics;
+
+USE DATABASE retail_analytics;
+
+CREATE OR REPLACE SCHEMA raw_layer;
+
+USE SCHEMA raw_layer;
