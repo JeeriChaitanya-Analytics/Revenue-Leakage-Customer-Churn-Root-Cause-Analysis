82da03d69c7a7b318a424bb7b8c82c1ec8e32a48
✅ Jeeri chaitanya <chaituchaitanya347@gmail.com>
2026-02-25 22:55:01 +0530

    Update 02_raw_table_creation.sql

--- SQL/02_raw_table_creation.sql
+++ SQL/02_raw_table_creation.sql
@@ -1,1 +1,22 @@
+-- =====================================================
+-- STEP 2: Raw Table Creation
+-- Stores original transactional data without modification
+-- =====================================================
 
+CREATE OR REPLACE TABLE retail_raw (
+
+    transaction_id        STRING,
+    transaction_date      STRING,
+    region                STRING,
+    product               STRING,
+    customer_id           INTEGER,
+    sales_amount          NUMBER(18,2),
+    cost_amount           NUMBER(18,2),
+    profit_reported       NUMBER(18,2),
+    discount_percentage   INTEGER,
+    payment_method        STRING
+
+);
+
+-- Validate row count
+SELECT COUNT(*) FROM retail_raw;
